70899be218c7419b636e581fcf510f2d247db425
moses <ezechukwuchibuikemoses@gmail.com>
2024-12-25 22:35:31 +0100

    fixes

--- src/sections/Home/Features.tsx
+++ src/sections/Home/Features.tsx
@@ -43,7 +43,7 @@ const mockdata = [
         image: 'https://images.unsplash.com/photo-1497655392221-e645087843da?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         title: 'Orphans',
         description:
-            `Providing care and resources to ensure orphans thrive and have access to a nurturing environment.`,
+            `Providing care to ensure orphans thrive and have access to a nurturing environment.`,
         action: 'Donate'
     },
     {
